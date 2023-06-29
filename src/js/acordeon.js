288da8a279e50e29e078cb88560e4f2d23399e4b
José Ricardo Carreira <jricardocarreira@gmail.com>
2023-06-29 09:30:08 -0300

    Exercício finalizado

--- src/js/acordeon.js
+++ src/js/acordeon.js
@@ -15,6 +15,26 @@ botaoTexto[0].addEventListener("click", function(){
 
 })
 
+botaoTexto[1].addEventListener("click", function(){
+
+    esconderTexto();
+    desselecionarTriangulo();
+
+    textoApresentado[1].classList.add("mostrar")
+    trianguloSeletor[1].classList.add("triangulo-clicado")
+
+})
+
+botaoTexto[2].addEventListener("click", function(){
+
+    esconderTexto();
+    desselecionarTriangulo();
+
+    textoApresentado[2].classList.add("mostrar")
+    trianguloSeletor[2].classList.add("triangulo-clicado")
+
+})
+
 function esconderTexto() {
     const botaoAmostra = document.getElementsByClassName("mostrar")
 
@@ -24,5 +44,5 @@ function esconderTexto() {
 function desselecionarTriangulo() {
     const trianguloSelecionado = document.getElementsByClassName("triangulo-clicado")
 
-    trianguloAmostra[0].classList.remove("triangulo-clicado")
+    trianguloSelecionado[0].classList.remove("triangulo-clicado")
 }
